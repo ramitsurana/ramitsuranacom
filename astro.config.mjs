dce415a9cc3404220694c3456d720744911e6ce3
ramitsurana <ramitsurana@gmail.com>
2023-03-03 02:57:33 +0530

    test path 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 
 export default defineConfig({
-    site: 'https://ramitsurana.github.io/ramitsuranacom/',
+    site: 'https://ramitsurana.github.io/ramitsuranacom',
     base: '/ramitsuranacom',
   })
