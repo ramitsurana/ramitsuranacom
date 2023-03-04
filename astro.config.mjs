c253120d637a9ac3e146b6f1cd339ba09f7c4f48
ramitsurana <ramitsurana@gmail.com>
2023-03-04 21:28:02 +0530

    testing image path change

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,4 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 
-export default defineConfig({
-    site: 'https://ramitsurana.github.io/ramitsuranacom',
-    base: '/ramitsuranacom',
-  })
+export default defineConfig({});
