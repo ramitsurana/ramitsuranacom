6c690e2a11a8c1a1421c50f2ba7a1a55a5bedb8a
ramitsurana <ramitsurana@gmail.com>
2023-03-05 00:14:38 +0530

    test remove config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 
-export default defineConfig({site: 'https://ramitsurana.github.io',
-base: '/ramitsuranacom'});
+// export default defineConfig({site: 'https://ramitsurana.github.io',
+// base: '/ramitsuranacom'});
+
+export default defineConfig({});
