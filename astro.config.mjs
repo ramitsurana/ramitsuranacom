8238cfacdf8130b7a65227ec2c07a975e03123a5
ramitsurana <ramitsurana@gmail.com>
2023-03-04 23:33:27 +0530

    test123

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 
 export default defineConfig({site: 'https://ramitsurana.github.io',
-base: '/ramitsuranacom',});
+base: '/ramitsuranacom'});
